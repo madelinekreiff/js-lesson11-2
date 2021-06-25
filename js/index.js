0c31c1542d6a07f8a44c2e2efab079e4a47f8dc6
madelinekreiff <madelinekreiff@gmail.com>
2021-06-24 17:39:12 -0700

    Created an API async function and another function to select random users, their country and their avatar photo to display on a web page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    const userResults = data["results"];
+    
+    displayUsers(userResults);
+};
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+    for (user of userResults) {
+        let country = user.location.country; // or user["location"]["country"];
+        let name = user.name.first; // or user["name"]["first"]
+        let imageUrl = user.picture.medium; // or user["picture"]["medium"]
+    
+        let userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+                <h3>${name}</h3>
+                <p>${country}</p>
+                <img src=${imageUrl} alt="User avatar">
+            `;
+        randomFolks.append(userDiv);
+    }
+};
+
+getData();
